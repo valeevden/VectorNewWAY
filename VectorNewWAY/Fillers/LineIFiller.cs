109c0541dd5ea551af67eb96409b8fbdee4e2b1d
Denis Valeev <valeevden@gmail.com>
2020-12-14 03:16:36 +0300

    Brush Filler Add

---
+++ VectorNewWAY/Fillers/LineIFiller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace VectorNewWAY.Fillers
+{
+    public class LineIFiller : IFiller
+    {
+        public void FillFigure(Pen pen, Graphics graphics, GraphicsPath Path)
+        {
+            graphics.DrawPath(pen, Path);
+        }
+    }
+}
